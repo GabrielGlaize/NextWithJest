3a0bea40186127be429785a3e941b8c6538ea93b
gab <gabrielglaize59@gmail.com>
2026-05-19 18:52:31 +0200

    merci opus4.6

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe("Home", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+      name: /vsy appel appel appel/i,
     });
 
     expect(heading).toBeInTheDocument();
